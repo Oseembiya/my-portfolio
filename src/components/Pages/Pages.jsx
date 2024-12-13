fb378cc717f8bea8d8e555750b78606b1d68f2e2
oseembiya <ozeembiya@gmail.com>
2024-12-13 16:29:00 +0000

    updating my website

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../common/navbar";
-import Home from "../Home/home";
+import Home from "../home/home";
+import About from "../about/about";
+import Experience from "../Esperience/Experience";
 
 export default function Pages() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
+      <Experience />
     </>
   );
 }
