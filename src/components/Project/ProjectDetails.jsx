91feb5fcf8a76454a370d0021818c9f51dae42c3
oseembiya <ozeembiya@gmail.com>
2024-12-21 17:58:15 +0000

    update

---
+++ src/components/Project/ProjectDetails.jsx
@@ -0,0 +1,66 @@
+const ProjectDetails = [
+  {
+    section: "Front-End Developer",
+    projects: [
+      {
+        projectName: "School Activities",
+        href: "https://oseembiya.github.io/Vue/",
+        image: "/src/assets/ProjectImage1.png",
+        tools: ["Vue.js", "Css", "Node.js", "AWS"],
+      },
+      {
+        projectName: "Portfolio Website",
+        href: "https://myportfolio.com",
+        image: "/src/assets/osee.jpeg",
+        tools: ["Vue.js", "Css"],
+      },
+    ],
+  },
+  {
+    section: "Back-End",
+    projects: [
+      {
+        projectName: "API Server",
+        href: "https://apiserver.com",
+        image: "/src/assets/ProjectImage1.png",
+        tools: ["Vue.js", "Css"],
+      },
+      {
+        projectName: "Database Management",
+        href: "https://dbmanager.com",
+        image: "/src/assets/businessMan.jpg",
+        tools: ["Vue.js", "Css"],
+      },
+    ],
+  },
+  {
+    section: "Software Engineer",
+    projects: [
+      {
+        projectName: "System Design",
+        href: "https://systemdesign.com",
+        image: "/src/assets/system.jpg",
+        tools: ["Vue.js", "Css"],
+      },
+    ],
+  },
+  {
+    section: "Ux-Design",
+    projects: [
+      {
+        projectName: "User Flow",
+        href: "https://userflow.com",
+        image: "/src/assets/userflow.jpg",
+        tools: ["Vue.js", "Css"],
+      },
+      {
+        projectName: "Wireframes",
+        href: "https://wireframes.com",
+        image: "/src/assets/wireframes.jpg",
+        tools: ["Vue.js", "Css"],
+      },
+    ],
+  },
+];
+
+export default ProjectDetails;
