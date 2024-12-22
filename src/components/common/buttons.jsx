9b4e7671e7925f0585b12b4379f347d28b52102e
oseembiya <ozeembiya@gmail.com>
2024-12-22 16:47:15 +0000

    update

--- src/components/common/buttons.jsx
+++ src/components/common/buttons.jsx
@@ -1,8 +1,8 @@
 // src/components/Common/Button.js
 // eslint-disable-next-line react/prop-types
-export default function Button({ label, type, onClick }) {
+export default function Button({ label, name, onClick }) {
   return (
-    <button className={`btn btn-${type}`} onClick={onClick}>
+    <button className={`btn btn-${name}`} onClick={onClick}>
       {label}
     </button>
   );
