c794937f8594697ac40c2658ab8c78254565fcef
oseembiya <ozeembiya@gmail.com>
2024-12-23 16:54:52 +0000

    updating my website

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,6 +1,6 @@
 import Home from "../home/home";
 import About from "../about/about";
-import Skills from "../common/skills";
+import Skills from "../common/SkillSet/skills";
 import Project from "../Project/Project";
 
 export default function Pages() {
