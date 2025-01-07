93e28ec6cd6b18be27e852ab0224f8cbe95f8e5a
oseembiya <ozeembiya@gmail.com>
2025-01-07 18:54:50 +0000

    updating my porfolio

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,16 +1,21 @@
+import Photo from "/src/assets/osee.jpeg";
+
+// eslint-disable-next-line react/prop-types
+function Button({ value }) {
+  return <button className="about-btn">{value}</button>;
+}
 export default function About() {
   return (
-    <div className="container-fluid about_2335">
+    <div className="container-fluid about-2335" id="about">
       <h4>Who I am</h4>
-      <div className="aboutImg_233" id="#about">
-        <div className="content_2335">
-          <div className="aboutMe_2335">
-            <article>Education</article>
-            <article>Experience</article>
-          </div>
+      <div className="aboutMe-2335">
+        <div className="content-2335">
+          <Button value="Eduction" />
+          <Button value="Experience " />
+          <div className="aboutImg_2335"></div>
         </div>
-        <div className="aboutImg_2335">
-          <img alt="AboutImage" />
+        <div className="aboutImg-2335">
+          <img className="Img-2335" src={Photo} alt="AboutImage" />
         </div>
       </div>
     </div>
