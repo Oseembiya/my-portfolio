c3d99aecd209b946d098ef359836fd49d13d2e89
oseembiya <ozeembiya@gmail.com>
2025-01-07 17:33:17 +0000

    updating

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,20 +1,17 @@
-import "./about.css";
-import AboutMe from "/src/assets/osee.jpeg";
 export default function About() {
   return (
     <div className="container-fluid about_2335">
-      <h1>Who I am</h1>
-      <img className="aboutImg_2335" src={AboutMe} alt="AboutImage" />
-      <div className="content_2335">
-        <p>
-          <strong>
-            <i className="fa fa-house"></i>Base in London
-          </strong>
-          <br />
-          Passionate and dedicated Front-End Developer, ready to tackle
-          challenges and create innovative, user-friendly web experiences that
-          drive results.
-        </p>
+      <h4>Who I am</h4>
+      <div className="aboutImg_233" id="#about">
+        <div className="content_2335">
+          <div className="aboutMe_2335">
+            <article>Education</article>
+            <article>Experience</article>
+          </div>
+        </div>
+        <div className="aboutImg_2335">
+          <img alt="AboutImage" />
+        </div>
       </div>
     </div>
   );
