08bf98924e333dd11c9c7f9b8d19cd1d718d07f5
oseembiya <ozeembiya@gmail.com>
2025-01-13 14:25:52 +0000

    updating

--- src/components/about/aboutMeData.jsx
+++ src/components/about/aboutMeData.jsx
@@ -3,16 +3,17 @@ const AboutMeDetails = [
     aboutMe: "Eduction",
     aboutMeDetails: [
       {
-        year: "2018-2020",
-        course: "Btec in information Technology ",
+        year: "2022-2025",
+        course: "Bachelor Degree In Computer Science",
       },
+
       {
         year: "2020-2022",
         course: "BTEC Level 3 In Information Technology ",
       },
       {
-        year: "2022-2025",
-        course: "Bachelor Degree In Computer Science",
+        year: "2018-2020",
+        course: "Btec in information Technology ",
       },
     ],
   },
